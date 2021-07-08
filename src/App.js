74bb0caab72f695ec2db823a478a864143bd984d
mina.nastaran1371 <mina.nastaran1371@gmail.com>
2021-07-08 23:22:41 +0430

    main pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Layout/Header'
+import Footer from './Layout/Footer'
+import BodyPages from './Layout/BodyPages'
+//style
+import './Style/shop_style.css'
+import { Route } from 'react-router'
 
 function App() {
   return (
-    <div className="App">
-      <h1> hi mina ...</h1>
-      <h1> are u ok ?</h1>
-    </div>
+    <>
+      <Header />
+        <BodyPages />
+        {/* <Route path='/' exact component={Home}/>
+            <Route path='/Products' exact component={Products}/>
+            <Route path='/Product/:id' exact component={ProductDetails}/> */}
+      <Footer />
+    </>
   );
 }
 
